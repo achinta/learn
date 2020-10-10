f9bdccc188b68315f9e856e885534e1c803973be
Ajay Chinta <ajay.chinta@gmail.com>
2020-10-10 10:18:36 +0530

    [add] counter screen

--- react-native/rn-starter/src/screens/ComponentsScreen.js
+++ react-native/rn-starter/src/screens/ComponentsScreen.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { View, StyleSheet, Text }from 'react-native';
 
 const ComponentsScreen = (props) => {
-    const name = "Ajay";
+    const name = "Ajayx";
     return <View>
-        <Text style={styles.textStyle}> Getting started with React Native</Text>
-<Text style={styles.subHeaderStyle}> My Name is {name}</Text>
+        <Text style={styles.textStyle}> Getting started withx React Native</Text>
+        <Text style={styles.subHeaderStyle}> My Name is {name}</Text>
     </View>
 }
 
 const styles = StyleSheet.create({
     textStyle: {
-        fontSize: 30
+        fontSize: 30 
     },
     subHeaderStyle: {
         fontSize: 20
--- react-native/rn-starter/src/screens/CounterScreen.js
+++ react-native/rn-starter/src/screens/CounterScreen.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, StyleSheet, Text, Button } from 'react-native';
 
 const CounterScreen = (props) => {
+    const [counter, setCounter] = useState(0);
+
     return <View>
-        <Button>Increase</Button>
-        <Button>Decrease</Button>
+        <Button 
+        title="Increase"
+        onPress={() => {
+            setCounter(counter + 1);
+        }} />
+        <Button 
+
+        title="Decrease"
+        onPress={() => {
+            setCounter(counter - 1);
+        }} />
+
+    <Text>current value{counter}</Text>
 
     </View>
 } 
 
-const 
+export default CounterScreen;
+
