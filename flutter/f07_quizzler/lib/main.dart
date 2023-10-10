da26bf8e456859867268f7bd86ebbce0ca129a28
Ajay Chinta <ajay.chinta@volunteer.heartfulness.org>
2023-10-10 12:21:07 +0530

    quizzlet works

--- flutter/f07_quizzler/lib/main.dart
+++ flutter/f07_quizzler/lib/main.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
+import 'question_bank.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
-void main() => runApp(Quizzler());
+QuestionBank questionBank = QuestionBank();
 
-class Quizzler extends StatelessWidget {
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.grey.shade900,
-        body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10.0),
-            child: QuizPage(),
-          ),
-        ),
-      ),
-    );
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Quizzler'),
+              backgroundColor: Colors.grey[900],
+            ),
+            body: SafeArea(
+              child: QuizPage(),
+            )));
   }
 }
 
@@ -25,6 +27,31 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  var scoreKeeper = <Icon>[];
+
+  void checkAnswer(bool userPickedAnswer) {
+    bool correctAnswer = questionBank.getQuestionAnswer();
+
+    setState(() {
+      if (userPickedAnswer == correctAnswer) {
+        scoreKeeper.add(Icon(Icons.check, color: Colors.green));
+      } else {
+        scoreKeeper.add(Icon(Icons.close, color: Colors.red));
+      }
+      if (questionBank.isFinished()) {
+        Alert(
+          context: context,
+          title: 'Finished!',
+          desc: 'You\'ve reached the end of the quiz.',
+        ).show();
+        questionBank.reset();
+        scoreKeeper = [];
+      } else {
+        questionBank.nextQuestion();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -33,65 +60,43 @@ class _QuizPageState extends State<QuizPage> {
       children: <Widget>[
         Expanded(
           flex: 5,
-          child: Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Center(
-              child: Text(
-                'This is where the question text will go.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 25.0,
-                  color: Colors.white,
-                ),
-              ),
-            ),
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text(questionBank.getQuestionText()),
+            // style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
           ),
         ),
         Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
-              child: Text(
-                'True',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                ),
-              ),
-              onPressed: () {
-                //The user picked true.
-              },
-            ),
+          flex: 1,
+          child: ElevatedButton(
+            onPressed: () {
+              setState(() {
+                checkAnswer(true);
+              });
+            },
+            child: Text('True'),
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
           ),
         ),
         Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
-              child: Text(
-                'False',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
-                ),
-              ),
-              onPressed: () {
-                //The user picked false.
-              },
-            ),
+          flex: 1,
+          child: ElevatedButton(
+            onPressed: () {
+              setState(() {
+                checkAnswer(false);
+              });
+            },
+            child: Text('False'),
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Expanded(
+          flex: 1,
+          child: Row(
+            children: scoreKeeper,
+          ),
+        )
       ],
     );
   }
 }
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
